af60bdda67dbf1d5f0a48eac3ff3d21ad3835f3a
EuMari <eunika.gielniewska@gmail.com>
2019-12-01 15:41:17 +0100

    dodalam opisy

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import unittest
 
 class PierwszyTest(unittest.TestCase):
-    def setUp(self):
+    def setUp(self)                         #setUp musi byc
         print("Przygotowanie do testu")
-    def test_224(self):
+    def test_224(self):                     #test moze sie nazywac dowolnie, ale musi sie zaczynac od "test"
         print("Sprawdzam, czy 2+2 =4..")
         assert(2+2==4)
-    def tearDown(self):
+    def tearDown(self):                     # tearDown musi byc
         print("Sprzatam po testach")
 
 if __name__ == '__main__':
