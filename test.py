cd80542da7c36758c6961ee5971b3d17c74b19a2
EuMari <eunika.gielniewska@gmail.com>
2019-12-01 15:54:13 +0100

    kolejne komentarze

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ class PierwszyTest(unittest.TestCase):
     def test_224(self):                     #test moze sie nazywac dowolnie, ale musi sie zaczynac od "test"
         print("Sprawdzam, czy 2+2 =4..")
         assert(2+2==4)
-    def tearDown(self):                     # tearDown musi byc
+    def tearDown(self):                     #tearDown musi byc
         print("Sprzatam po testach")
 
-if __name__ == '__main__':
+if __name__ == '__main__':                  #odpal test tylko z tego pliku; jezeli bedzie gdzies zaimportowane, to test sie nie uruchomi
     unittest.main(verbosity=2)
